c0789e57e62e958d0cee80199c278a898963dfd1
abdulfatiransari <76064480+abdulfatiransari@users.noreply.github.com>
2022-09-10 08:55:17 +0500

    Create vote.sol

---
+++ vote.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+contract voting{
+
+    address public admin;
+    constructor() {
+        admin = (msg.sender);
+    }
+
+    struct Voter {
+        string name;
+        address _address;
+    }
+
+    // mapping(address => bool) whitelistedAddresses;
+    mapping(address=>bool) isBlacklisted;
+
+    modifier onlyadmin() {
+        require(msg.sender == admin,"Your are not Admin");
+        _;
+    }
+
+    Voter[] public voter;
+
+    function vote (string memory _name, address _address) external {
+        voter.push(Voter(_name, _address));
+        
+    }
+
+    // function addUser(address _addressToWhitelist) public onlyadmin {
+    //     whitelistedAddresses[_addressToWhitelist] = true;
+    // }
+
+    // function verifyUser(address _whitelistedAddress) public view returns(bool) {
+    //     bool userIsWhitelisted = whitelistedAddresses[_whitelistedAddress];
+    //     return userIsWhitelisted;
+    // }
+
+
+    function blackList(address _user) public onlyadmin {
+        require(!isBlacklisted[_user], "user already blacklisted");
+        isBlacklisted[_user] = true;
+    }
+    
+    function whitelist(address _user) public onlyadmin {
+        require(isBlacklisted[_user], "user already whitelisted");
+        isBlacklisted[_user] = false;
+    }
+    
+}
